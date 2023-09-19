4fc9871f8d22bfdfcfc1f98dced5deb950e38dba
Minjun97 <piaominjun1997@gmail.com>
2023-09-19 15:15:14 +0900

    setting menu

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:capstone_dingle/mainpage.dart';
+import 'package:capstone_dingle/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
